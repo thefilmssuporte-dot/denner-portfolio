214ea427af880b4921cd588a705ebaaf2c9fcff9
appcostura9-crypto <appcostura9@gmail.com>
2026-09-13 13:37:40 -0300

    Teste hero H264 mobile

--- app/components/HeroVideo.tsx
+++ app/components/HeroVideo.tsx
@@ -4,7 +4,7 @@ export default function HeroVideo() {
   return (
     <video
       className="hero-video"
-      src="https://res.cloudinary.com/wtjfrix6/video/upload/TBT_RODEIO_2024.mp4"
+      src="https://res.cloudinary.com/wtjfrix6/video/upload/v1789267924/Modelo_institucional_1.mp4"
       autoPlay
       muted
       loop
